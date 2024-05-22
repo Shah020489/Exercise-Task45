8aae84f833cc3a4f33a7dbc58cda6585bc119d59
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-05-22 18:23:51 +0500

    changes are added.

---
+++ task14.js
@@ -0,0 +1,9 @@
+/**
+ * Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?
+ *  Make a list that includes at least three people you’d like to invite to dinner.
+ * Then use your list to print a message to each person, inviting them to dinner.
+ */
+var guestInviteList = ['Shakeel', 'Ahmed', 'Shahid'];
+console.log("Hello " + guestInviteList[0] + " you are invited to dinner");
+console.log("Hello " + guestInviteList[1] + " you are invited to dinner");
+console.log("Hello " + guestInviteList[2] + " you are invited to dinner");
---
+++ task15.ts
@@ -0,0 +1,27 @@
+/**
+ * Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+ * so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+• Start with your program from Exercise 14. Add a print statement at the end of your
+ program stating the name of the guest who can’t make it.
+
+• Modify your list, replacing the name of the guest who can’t make it with the name 
+of the new person you are inviting.
+
+• Print a second set of invitation messages, one for each person who is still in your list.
+ * 
+ */
+
+
+var guestInviteFirstList = ['Shakeel', 'Ahmed', 'Shahid'];
+console.log("Hello " + guestInviteFirstList[0] + " you are invited to dinner");
+console.log("Hello " + guestInviteFirstList[1] + " you are invited to dinner");
+console.log("Hello " + guestInviteFirstList[2] + " you are invited to dinner");
+console.log("Sorry " + guestInviteFirstList[2] + " you can't make it");
+
+
+
+let guestInviteSecondList=['Jameel','Wazeer','Rizwan'];
+console.log("Hello " + guestInviteSecondList[0] + " you are invited to dinner");
+console.log("Hello " + guestInviteSecondList[1] + " you are invited to dinner");
+console.log("Hello " + guestInviteSecondList[2] + " you are invited to dinner");
